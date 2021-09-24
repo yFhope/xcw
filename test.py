17d78c4427636b321403a2be7ec93db3d5c07619
PD_YW <yuanwei@singhand.com>
2021-09-24 16:51:49 +0800

    数据提取完成，待编写数据库程序

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from openpyxl import load_workbook
 
-workbook = load_workbook(filename='../4A5A汇总.xlsx')
+workbook = load_workbook(filename='xc/45A.xlsx')
 sheet = workbook.active
-names = sheet['C']  # 景点名称
+names = sheet['G']  # 景点名称
 ids = sheet['G']  # 景点ID
 urls = sheet['F']  # 景点url
 add_params = {}
 for name, jid, url in zip(names[1:30], ids[1:30], urls[1:30]):
-    add_params['j_name'] = name.value  # 景点名称
-    add_params['j_id'] = jid.value  # 景点ID
-    j_url = jid.value # 景点url
-    print(j_url,add_params)
+    # add_params['j_name'] = name.value  # 景点名称
+    # add_params['j_id'] = jid.value  # 景点ID
+    jid = name.value # 景点url
+    print(jid)
--- xc/spiders/xcw.py
+++ xc/spiders/xcw.py
@@ -1,4 +1,7 @@
+import json
+
 import scrapy
+from jsonpath import jsonpath
 from openpyxl import load_workbook
 
 
@@ -7,21 +10,58 @@ class XcwSpider(scrapy.Spider):
 
     allowed_domains = ['ctrip.com']
 
+    comment_params = {"arg": {"resourceId": 26184, "resourceType": 11, "pageIndex": 1, "pageSize": 10, "sortType": 3,
+                              "commentTagId": -1, "collapseType": 1, "channelType": 7, "videoImageSize": "700_392",
+                              "starType": 0},
+                      "head": {"cid": "09031133417600395476", "ctok": "", "cver": "1.0", "lang": "01", "sid": "8888",
+                               "syscode": "09", "auth": None, "extension": [{"name": "protocal", "value": "https"}]},
+                      "contentType": "json"}
+
+    source_params = {"arg": {"resourceId": 5078351, "resourceType": 11},
+                     "head": {"cid": "09031173417826624428", "ctok": "", "cver": "1.0", "lang": "01", "sid": "8888",
+                              "syscode": "09", "auth": None, "extension": [{"name": "protocal", "value": "https"}]},
+                     "contentType": "json"}
+
+    comment_api = "https://m.ctrip.com/restapi/soa2/13444/json/getCommentCollapseList"
+    source_api = "https://m.ctrip.com/restapi/soa2/13444/json/getPoiCommentInfoWithHotTag"
+
     # url = 'https://gs.ctrip.com/html5/you/sight/huangshanscenicarea19/{}.html'
 
     def start_requests(self):
-        workbook = load_workbook(filename='../4A5A汇总.xlsx')
+        workbook = load_workbook(filename=r'C:\Users\windows\Desktop\xcw\45A.xlsx')
         sheet = workbook.active
         names = sheet['C']  # 景点名称
         ids = sheet['G']  # 景点ID
         urls = sheet['F']  # 景点url
-        for name, jid, url in zip(names[2:30], ids[2:30], urls[2:30]):
+        for name, jid, url in zip(names[1:5], ids[1:5], urls[1:5]):
             add_params = {}
             add_params['J_name'] = name.value  # 景点名称
             add_params['J_id'] = jid.value  # 景点ID
-            j_url = jid.value  # 景点url
-            yield scrapy.Request(url=j_url, callback=self.parse_data, cb_kwargs=add_params)
+            j_url = url.value  # 景点url
+            yield scrapy.Request(url=j_url, callback=self.parse_index_data, cb_kwargs=add_params)
 
-    def parse_data(self, response, J_name, J_id):
-        print(response)
-        print('---' * 32)
+    def parse_index_data(self, response, J_name, J_id):
+        j_kargs = {}
+        j_kargs['J_name'] = J_name
+        j_kargs['J_id'] = J_id
+        j_kargs['level'] = response.xpath('//span[@class="titleTag"]//text()').extract_first()  # 等级 4A 5A
+        j_kargs['overallScore'] = response.xpath('//span[@class="commentNum"]/text()').extract_first()  # 总体评分
+        j_kargs['label'] = "".join(response.xpath('//div[@class="shortFeatures"]//text()').extract())  # 景区类型  标签
+
+        self.source_params['arg']['resourceId'] = J_id
+        yield scrapy.Request(self.source_api, callback=self.get_comm_api, method='POST',
+                             body=json.dumps(self.source_params), headers={'Content-Type': 'application/json'},
+                             cb_kwargs=j_kargs)
+
+    def get_comm_api(self, response, J_name, J_id, level, overallScore, label):
+        json_data = json.loads(response.text)
+        totalCount = jsonpath(json_data, '$..poiInfo.commentCount')[0]  # 评论数量
+        js, qw, xjb = jsonpath(json_data, '$..scores..score')  # 分维度评分
+        print("ID ", J_id)
+        print("名称 ", J_name)
+        print("等级 ", level)
+        print("总评分 ", overallScore)
+        print("标签 ", label)
+        print("评论数 ", totalCount)
+        print(js, qw, xjb)
+        print("===" * 45)
